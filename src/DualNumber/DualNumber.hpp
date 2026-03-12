aefeff866253b8203e9fcebac44ea8236ecc6cc4
willmo3 <thelordofbouge@gmail.com>
2026-03-12 12:15:53 -0400

    Add additional dual number accessors

--- src/DualNumber/DualNumber.hpp
+++ src/DualNumber/DualNumber.hpp
@@ -30,7 +30,7 @@ public:
      * @param primal Primal (function) value.
      * @param deriv  Derivative value.
      */
-    DualNumber(T primal, T deriv): primal_value(primal), deriv_value(deriv) {}
+    DualNumber(T primal, T deriv): _primal_value(primal), _deriv_value(deriv) {}
 
     /*
      * Accessors
@@ -40,19 +40,47 @@ public:
      * @return Radius of the primal value.
      */
     double radius() const {
-        return primal_value.radius();
+        return _primal_value.radius();
     }
     /**
      * @return Minimum of the primal value.
      */
     double min() const {
-        return primal_value.min();
+        return _primal_value.min();
     }
     /**
      * @return Maximum of the primal value.
      */
     double max() const {
-        return primal_value.max();
+        return _primal_value.max();
+    }
+
+    /**
+     * @return A reference to the primal value. Less expensive, but limited lifetime.
+     */
+    const T &primal_ref() const {
+        return _primal_value;
+    }
+    /**
+     * @return A reference to the derivative value. Less expensive, but limited lifetime.
+     */
+    const T &deriv_ref() const {
+        return _deriv_value;
+    }
+
+    /**
+     * @return A copy of the primal value.
+     * Note that this is shallow, and heap resources -- i.e. vector contents -- will be shared.
+     */
+    T primal_value() {
+        return _primal_value;
+    }
+    /**
+     * @return a deep copy of the derivative value.
+     * Note that this is shallow, and heap resources -- i.e. vector contents -- will be shared.
+     */
+    T deriv_value() {
+        return _deriv_value;
     }
 
     /*
@@ -63,19 +91,19 @@ public:
      *   Division             : quotient rule
      */
     DualNumber operator+(const DualNumber& rhs) const {
-        return { primal_value + rhs.primal_value, deriv_value + rhs.deriv_value };
+        return { _primal_value + rhs._primal_value, _deriv_value + rhs._deriv_value };
     }
     DualNumber operator-(const DualNumber& rhs) const {
-        return { primal_value - rhs.primal_value, deriv_value - rhs.deriv_value };
+        return { _primal_value - rhs._primal_value, _deriv_value - rhs._deriv_value };
     }
     DualNumber operator*(const DualNumber& rhs) const {
-        return { primal_value * rhs.primal_value,
-                 deriv_value * rhs.primal_value + primal_value * rhs.deriv_value };
+        return { _primal_value * rhs._primal_value,
+                 _deriv_value * rhs._primal_value + _primal_value * rhs._deriv_value };
     }
     DualNumber operator/(const DualNumber& rhs) const {
         return {
-            primal_value / rhs.primal_value,
-            (deriv_value * rhs.primal_value - primal_value * rhs.deriv_value) / rhs.primal_value.pow(2u)
+            _primal_value / rhs._primal_value,
+            (_deriv_value * rhs._primal_value - _primal_value * rhs._deriv_value) / rhs._primal_value.pow(2u)
         };
     }
 
@@ -83,16 +111,16 @@ public:
      * DualNumber–scalar arithmetic
      */
     DualNumber operator+(double scalar) const {
-        return { primal_value + scalar, deriv_value };
+        return { _primal_value + scalar, _deriv_value };
     }
     DualNumber operator-(double scalar) const {
-        return { primal_value - scalar, deriv_value };
+        return { _primal_value - scalar, _deriv_value };
     }
     DualNumber operator*(double scalar) const {
-        return { primal_value * scalar, deriv_value * scalar };
+        return { _primal_value * scalar, _deriv_value * scalar };
     }
     DualNumber operator/(double scalar) const {
-        return { primal_value / scalar, deriv_value / scalar };
+        return { _primal_value / scalar, _deriv_value / scalar };
     }
 
     /*
@@ -107,22 +135,22 @@ public:
     DualNumber pow(uint32_t n) const {
         if (n == 0) {
             // f^0 = 1 (constant), derivative = 0
-            return DualNumber(primal_value.pow(0), deriv_value * 0.0);
+            return DualNumber(_primal_value.pow(0), _deriv_value * 0.0);
         }
 
-        return { primal_value.pow(n), primal_value.pow(n - 1) * static_cast<double>(n) * deriv_value };
+        return { _primal_value.pow(n), _primal_value.pow(n - 1) * static_cast<double>(n) * _deriv_value };
     }
     /**
      * @return A new dual number representing the absolute value, d/dx(|f|) = sign(f) * f'.
      */
     DualNumber abs() const {
-        if (primal_value >= 0.0) {
-            return { primal_value.abs(), deriv_value };
-        } else if (primal_value < 0.0) {
-            return { primal_value.abs(), deriv_value * -1.0 };
+        if (_primal_value >= 0.0) {
+            return { _primal_value.abs(), _deriv_value };
+        } else if (_primal_value < 0.0) {
+            return { _primal_value.abs(), _deriv_value * -1.0 };
         }
         // Mixed sign (e.g. an interval spanning zero): conservative bound.
-        return { primal_value.abs(), deriv_value.abs() };
+        return { _primal_value.abs(), _deriv_value.abs() };
     }
 
     /*
@@ -133,17 +161,17 @@ public:
      * @param rhs Other DualNumber to union with.
      * @return A new DualNumber whose components are the respective unions.
      */
-    DualNumber union_with(const DualNumber& rhs) const {
-        return { primal_value.union_with(rhs.primal_value),
-                 deriv_value.union_with(rhs.deriv_value) };
+    DualNumber union_with(const DualNumber &rhs) const {
+        return { _primal_value.union_with(rhs._primal_value),
+                 _deriv_value.union_with(rhs._deriv_value) };
     }
     /**
      * @param n_splits Number of splits to produce.
      * @return Vector of n_splits DualNumbers.
      */
     std::vector<DualNumber> split(uint32_t n_splits) const {
-        auto primal_splits = primal_value.split(n_splits);
-        auto deriv_splits  = deriv_value.split(n_splits);
+        auto primal_splits = _primal_value.split(n_splits);
+        auto deriv_splits  = _deriv_value.split(n_splits);
 
         std::vector<DualNumber> result;
         result.reserve(n_splits);
@@ -160,23 +188,23 @@ public:
      * Equality (==, !=) compares both primal and derivative.
      * Ordering (<, <=, >, >=) is determined by the primal value alone.
      */
-    bool operator==(const DualNumber& rhs) const {
-        return primal_value == rhs.primal_value && deriv_value == rhs.deriv_value;
+    bool operator==(const DualNumber &rhs) const {
+        return _primal_value == rhs._primal_value && _deriv_value == rhs._deriv_value;
     }
-    bool operator!=(const DualNumber& rhs) const {
-        return primal_value != rhs.primal_value || deriv_value != rhs.deriv_value;
+    bool operator!=(const DualNumber &rhs) const {
+        return _primal_value != rhs._primal_value || _deriv_value != rhs._deriv_value;
     }
-    bool operator<(const DualNumber& rhs) const {
-        return primal_value < rhs.primal_value;
+    bool operator<(const DualNumber &rhs) const {
+        return _primal_value < rhs._primal_value;
     }
-    bool operator<=(const DualNumber& rhs) const {
-        return primal_value <= rhs.primal_value;
+    bool operator<=(const DualNumber &rhs) const {
+        return _primal_value <= rhs._primal_value;
     }
-    bool operator>(const DualNumber& rhs) const {
-        return primal_value > rhs.primal_value;
+    bool operator>(const DualNumber &rhs) const {
+        return _primal_value > rhs._primal_value;
     }
-    bool operator>=(const DualNumber& rhs) const {
-        return primal_value >= rhs.primal_value;
+    bool operator>=(const DualNumber &rhs) const {
+        return _primal_value >= rhs._primal_value;
     }
 
     /*
@@ -185,35 +213,35 @@ public:
      * Comparison is against the primal value only
      */
     bool operator<(double scalar) const {
-        return primal_value < scalar;
+        return _primal_value < scalar;
     }
     bool operator<=(double scalar) const {
-        return primal_value <= scalar;
+        return _primal_value <= scalar;
     }
     bool operator>(double scalar) const {
-        return primal_value > scalar;
+        return _primal_value > scalar;
     }
     bool operator>=(double scalar) const {
-        return primal_value >= scalar;
+        return _primal_value >= scalar;
     }
 
     /*
      * Utility functions
      */
     template<class Archive>
-    void serialize(Archive& archive) {
-        archive(cereal::make_nvp("primal", primal_value),
-                cereal::make_nvp("deriv",  deriv_value));
+    void serialize(Archive &archive) {
+        archive(cereal::make_nvp("primal", _primal_value),
+                cereal::make_nvp("deriv",  _deriv_value));
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const DualNumber& dn) {
-        os << "DualNumber(primal=" << dn.primal_value << ", deriv=" << dn.deriv_value << ")";
+    friend std::ostream &operator<<(std::ostream &os, const DualNumber &dn) {
+        os << "DualNumber(primal=" << dn._primal_value << ", deriv=" << dn._deriv_value << ")";
         return os;
     }
 
 private:
-    T primal_value;
-    T deriv_value;
+    T _primal_value;
+    T _deriv_value;
 };
 
 #endif //DIFFDOMAIN_DUALNUMBER_H
